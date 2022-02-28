ffe9dcb5f0adf82d3f8c2924dd1b25561a38b802
Billy Laws <blaws05@gmail.com>
2022-02-28 19:13:27 +0000

    Add mprotect calls for BTI/--X devices

--- android_linker_ns.cpp
+++ android_linker_ns.cpp
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #include <android/dlext.h>
 #include <android/log.h>
+#include <sys/mman.h>
 #include "elf_soname_patcher.h"
 #include "android_linker_ns.h"
 
@@ -119,6 +120,10 @@ void *linkernsbypass_namespace_dlopen_unique(const char *libPath, const char *li
     return android_dlopen_ext(PathBuf.data(), flags, &hookExtInfo);
 }
 
+static void *align_ptr(void *ptr) {
+    return reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(ptr) & ~(PAGE_SIZE - 1));
+}
+
 /* Private */
 __attribute__((constructor)) static void resolve_linker_symbols() {
     using loader_dlopen_t = void *(*)(const char *, int, const void *);
@@ -139,6 +144,9 @@ __attribute__((constructor)) static void resolve_linker_symbols() {
         };
         static_assert(sizeof(BranchLinked) == 4, "BranchLinked is wrong size");
 
+        // Some devices ship with --X mapping for exexecutables so work around that
+        mprotect(align_ptr(reinterpret_cast<void *>(&dlopen)), PAGE_SIZE, PROT_READ | PROT_EXEC);
+
         // dlopen is just a wrapper for __loader_dlopen that passes the return address as the third arg hence we can just walk it to find __loader_dlopen
         auto blInstr{reinterpret_cast<BranchLinked *>(&dlopen)};
         while (!blInstr->Verify())
@@ -146,6 +154,9 @@ __attribute__((constructor)) static void resolve_linker_symbols() {
 
         return reinterpret_cast<loader_dlopen_t>(blInstr + blInstr->offset);
     }()};
+
+    // Protect the loader_dlopen function to remove the BTI attribute (since this is an internal function that isn't intended to be jumped indirectly to)
+    mprotect(align_ptr(reinterpret_cast<void *>(&loader_dlopen)), PAGE_SIZE, PROT_READ | PROT_EXEC);
 
     // Passing dlopen as a caller address tricks the linker into using the internal unrestricted namespace letting us access libraries that are normally forbidden in the classloader namespace imposed on apps
     auto ldHandle{loader_dlopen("ld-android.so", RTLD_LAZY, reinterpret_cast<void *>(&dlopen))};
